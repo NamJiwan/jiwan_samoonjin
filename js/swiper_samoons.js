170555b1919648bb0f22aaf04fbbee37244759d6
NamJiwan <129821189+NamJiwan@users.noreply.github.com>
2023-07-23 20:34:20 +0900

    about_tap_carousel_make

--- js/swiper_samoons.js
+++ js/swiper_samoons.js
@@ -1,9 +1,13 @@
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper(".swiper_event", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
   slidesPerView: 3,
   spaceBetween: 10,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   // Responsive breakpoints
   breakpoints: {
     // when window width is >= 320px
@@ -37,4 +41,22 @@ const swiper = new Swiper(".swiper", {
   // scrollbar: {
   //   el: ".swiper-scrollbar",
   // },
+});
+
+// about carousel
+var swiper3 = new Swiper(".mySwiper", {
+  spaceBetween: 20,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  thumbs: {
+    swiper: swiper3,
+  },
 });
